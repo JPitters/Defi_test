59852379b4565a05120594f64bb79c79aa75087b
Jordan Pitters <jordan_pitters@hotmail.com>
2023-02-15 18:20:33 -0500

    minor additions to staking, issuing, and testing

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -7,6 +7,7 @@ import "./DaiToken.sol";
 contract TokenFarm {
     // Code goes here
     string public name = "Jook Token Farm";
+    address public owner;
     JookToken public jookToken;
     DaiToken public daiToken; //saved as a state variable
 
@@ -21,10 +22,14 @@ contract TokenFarm {
         //1
         jookToken = _jookToken;
         daiToken = _daiToken;
+        owner = msg.sender;
     }
 
     // 1. Stake Tokens (Deposit)
     function stakeTokens(uint _amount) public {
+        
+        //require amount greater than 0
+        require(_amount > 0, "amount cannot be 0 or less than 0");
         
         // Tranfer DAI tokens to this contract's address for staking
         daiToken.transferFrom(msg.sender, address(this), _amount);
@@ -42,7 +47,20 @@ contract TokenFarm {
         hasStaked[msg.sender] = true;
     }
 
-    // 2. Unstaking tokens (Withdraw)
-    // 3. Issuing Tokens
+    //  Issuing Tokens
+    function issueTokens() public {
+        //
+        require(msg.sender == owner, "Token provider must be administrative owner");
+
+        for (uint i=0; i < stakers.length; i++){
+            address recipient = stakers[i];
+            uint balance = stakingBalance[recipient];
+            if (balance > 0) {
+                jookToken.transfer(recipient, balance);
+            }
+        }
+    }
+
+    //  Unstaking tokens (Withdraw)
 
 }
